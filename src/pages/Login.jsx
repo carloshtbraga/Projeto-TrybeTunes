1538ebe8a8c92c21f5b6a4df38902e935081e049
Carlos <carloshtbraga@gmail.com>
2022-12-22 00:02:29 -0300

    Requisito 5
    Crie o formulário para pesquisar artistas

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -12,7 +12,7 @@ class Login extends React.Component {
   };
 
   buttonValidation = () => {
-    // Essa função faz a validacao do botao, eu pego o name do estado e faço uma variavel que retorna booleano, essa variavel confere se o .length do name é maior ou igual a 3. Caso de certo retorna true, aí eu inverto isso com o ! para que o estado do isButtonDisabled fique falso e o botao libere
+    // Essa função faz a validacao do botao, eu pego o nameInput do estado e faço uma variavel que retorna booleano, essa variavel confere se o .length do nameInput é maior ou igual a 3. Caso de certo retorna true, aí eu inverto isso com o ! para que o estado do isButtonDisabled fique falso e o botao libere
     const { name } = this.state;
     const three = 3;
     const higherThanThree = name.length >= three;
@@ -22,7 +22,7 @@ class Login extends React.Component {
   };
 
   handleChange = ({ target }) => {
-    // Essa e a funcao basica que input de texto ela pega o valor do que está sendo digitado no input e coloca no estado a cada mudança que ocorre e ao mesmo tempo a cada mudança ela roda a função de validacao do botao
+    // Essa e a funca basica que input de texto ela pega o valor do que está sendo digitado no input e coloca no estado a cada mudança que ocorre e ao mesmo tempo a cada mudança ela roda a função de validacao do botao
     const { value } = target;
     this.setState(
       {
@@ -33,7 +33,7 @@ class Login extends React.Component {
   };
 
   handleClick = () => {
-    // aqui eu pego o hsitory que vem automatico como props la do Route pois nele ficam salvas as rotas que eu passei e tbm o name do state depois, coloco o estado para ficar como loading, no segundo parametro do setstate eu uso a callback para que o que vem depois só aconteça após o primeiro parametro ocorrer que no caso é o de mudar o estado do loading. Entao caso o name exista(ele foi colcoado no input text) a gente roda a funcao que ja vem pronta pra criar um usuario com esse nome. Depois ela leva a gente para a pagina search.
+    // aqui eu pego o hsitory que vem automatico como props la do Route pois nele ficam salvas as rotas que eu passei e tbm o nameInput do state depois, coloco o estado para ficar como loading, no segundo parametro do setstate eu uso a callback para que o que vem depois só aconteça após o primeiro parametro ocorrer que no caso é o de mudar o estado do loading. Entao caso o nameInput exista(ele foi colcoado no input text) a gente roda a funcao que ja vem pronta pra criar um usuario com esse nome. Depois ela leva a gente para a pagina search.
     const { history } = this.props;
     const { push } = history;
     const { name } = this.state;
@@ -57,11 +57,11 @@ class Login extends React.Component {
         {/* Aqui eu faco um ternario de apenas um retorno, por isso uso o &&. Caso o estado isLoading esteja como true, ele vai renderizar o componente Loading */}
         {isLoading && <Loading /> }
         <div data-testid="page-login">
-          <label htmlFor="nameInput">
+          <label htmlFor="name">
             <input
               type="text"
-              name="nameInput"
-              id="nameInput"
+              name="name"
+              id="name"
               data-testid="login-name-input"
               value={ name }
               onChange={ this.handleChange }
